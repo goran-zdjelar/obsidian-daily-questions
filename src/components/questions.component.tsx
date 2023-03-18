ab37be68bbbddb0bc3f7f488e08f64cc9765c894
Goran Zdjelar <goran.z@telesystems.hr>
2023-03-18 20:43:43 +0100

    Add questions for a random note

--- src/components/questions.component.tsx
+++ src/components/questions.component.tsx
@@ -2,46 +2,17 @@ import { Notice } from "obsidian";
 import * as React from "react";
 import { getQuestionFromOpenAi } from "../ai";
 import { useApp } from "../hooks";
+import { getRandomNumber } from "../utils";
 
 export function Questions() {
 	const app = useApp();
-	const [isLoading, setIsLoading] = React.useState(true);
+	const [isLoading, setIsLoading] = React.useState(false);
 	const [hasErrorOccurred, setHasErrorOcurred] = React.useState(false);
 	const [questions, setQuestions] = React.useState<String[]>([]);
-
-	React.useEffect(() => {
-		async function fetchFileContent() {
-			const currentFile = app?.workspace.getActiveFile();
-
-			if (!currentFile) {
-				new Notice("There was an issue while reading current note");
-				setHasErrorOcurred(true);
-				return;
-			}
-
-			const currentFileContent = await app?.vault.read(currentFile);
-
-			if (!currentFileContent) {
-				new Notice(
-					"There was an issue while reading current note content"
-				);
-				setHasErrorOcurred(true);
-				return;
-			}
-
-			const aiQuestions = await getQuestionFromOpenAi(currentFileContent);
-
-			setIsLoading(false);
-			setHasErrorOcurred(false);
-			setQuestions(aiQuestions);
-		}
-
-		fetchFileContent();
-	}, []);
+	const [relatedTo, setRelatedTo] = React.useState<String>("");
 
 	return (
 		<div>
-			<h4>Questions:</h4>
 			{isLoading && !hasErrorOccurred ? (
 				<p>Preparing questions...</p>
 			) : null}
@@ -53,14 +24,104 @@ export function Questions() {
 			) : null}
 
 			{questions.length > 0 && !isLoading && !hasErrorOccurred ? (
-				<ol>
-					{questions.map((question, index) => (
-						<li key={index} style={{ marginBottom: "10px" }}>
-							{question}
-						</li>
-					))}
-				</ol>
+				<>
+					<h4>
+						Questions {relatedTo ? `related to "${relatedTo}"` : ""}
+						:
+					</h4>
+
+					<ol>
+						{questions.map((question, index) => (
+							<li key={index} style={{ marginBottom: "10px" }}>
+								{question}
+							</li>
+						))}
+					</ol>
+				</>
 			) : null}
+
+			<div>
+				<button
+					onClick={async () => {
+						setIsLoading(true);
+						setHasErrorOcurred(false);
+
+						const currentFile = app?.workspace.getActiveFile();
+
+						if (!currentFile) {
+							new Notice(
+								"There was an issue while reading current note"
+							);
+							setHasErrorOcurred(true);
+							return;
+						}
+
+						const currentFileContent = await app?.vault.read(
+							currentFile
+						);
+
+						if (!currentFileContent) {
+							new Notice(
+								"There was an issue while reading current note content"
+							);
+							setHasErrorOcurred(true);
+							return;
+						}
+
+						const aiQuestions = await getQuestionFromOpenAi(
+							currentFileContent
+						);
+
+						setIsLoading(false);
+						setHasErrorOcurred(false);
+						setQuestions(aiQuestions);
+					}}
+				>
+					Generate quiz questions for this note
+				</button>
+				<button
+					onClick={async () => {
+						setQuestions([]);
+						setIsLoading(true);
+						setHasErrorOcurred(false);
+
+						const allNotes = app?.vault.getMarkdownFiles();
+						const randomNote =
+							allNotes?.[getRandomNumber(0, allNotes.length - 1)];
+
+						if (!randomNote) {
+							new Notice(
+								"There was an issue while reading current note"
+							);
+							setHasErrorOcurred(true);
+							return;
+						}
+
+						const currentFileContent = await app?.vault.read(
+							randomNote
+						);
+
+						if (!currentFileContent) {
+							new Notice(
+								"There was an issue while reading current note content"
+							);
+							setHasErrorOcurred(true);
+							return;
+						}
+
+						const aiQuestions = await getQuestionFromOpenAi(
+							currentFileContent
+						);
+
+						setRelatedTo(randomNote.basename);
+						setIsLoading(false);
+						setHasErrorOcurred(false);
+						setQuestions(aiQuestions);
+					}}
+				>
+					Generate quiz questions for a random note
+				</button>
+			</div>
 		</div>
 	);
 }
